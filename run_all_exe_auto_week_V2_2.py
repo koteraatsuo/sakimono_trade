04af31853a38c46d7ebf5849aec903b4ca88bbdb
kotera atsuo <k.atsuo-jp@outlokk.com>
2025-09-03 09:54:20 +0900

    updfate

--- run_all_exe_auto_week_V2_2.py
+++ run_all_exe_auto_week_V2_2.py
@@ -10,6 +10,8 @@ def exe_japanese_stocks():
     scripts_list = [
         ("C:/workspace/nihon_kabu_trade", "nihon_ver1.12_open_v4_load_sim_bigcompany.py"),
         ("C:/workspace/nihon_kabu_trade", "nihon_ver1.12_open_v4_load_sim_nomalcompany.py"),
+        ("C:/workspace/nihon_kabu_trade", "nihon_ver1.12_open_v4_load_sim_bigcompany_to60.py"),
+        ("C:/workspace/nihon_kabu_trade", "nihon_ver1.12_open_v4_load_sim_nomalcompany_to60.py"),
         ("C:/workspace/nihon_kabu_trade", "nihon_3top100単位_機械_S_emsemble_today_ver1.08_top30_load_sim_Fernandes.py"),
         ("C:/workspace/nihon_kabu_trade", "nihon_3top100単位_機械_S_emsemble_today_ver1.07_top30_load_sim_Fernandes.py"),
         ("C:/workspace/nihon_kabu_trade", "nihon_ver1.12_open_v4_load_sim.py"),
