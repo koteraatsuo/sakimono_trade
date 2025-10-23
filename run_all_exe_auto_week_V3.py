6fed0f966f145279b6519b684ba07cb2ee41c46d
kotera atsuo <k.astuo-jp@outlook.com>
2025-10-23 21:20:22 +0900

    update

--- run_all_exe_auto_week_V3.py
+++ run_all_exe_auto_week_V3.py
@@ -109,7 +109,7 @@ def exe_before_cfd_scripts():
 
 
     scripts_list = [
-        ("C:/workspace/cfd_trade", "nihon_ver1.12_open_v5_before_sim_ansemble_bigcompany.py"),
+        ("C:/workspace/cfd_trade", "nihon_ver1.12_open_v5_before_sim_ansemble5_bigcompany.py"),
         # ("C:/workspace/cfd_trade", "america_ver1.12_open_v5_before_train_4000.py"),
         # ("C:/workspace/cfd_trade", "cfd_america_ver1.10_short_open_v4.py"),
         # ("C:/workspace/cfd_trade", "cfd_america_ver1.10_open_v4_lev_2.py"),
@@ -140,8 +140,8 @@ def exe_cfd_scripts():
 
     scripts_list = [
         # ("C:/workspace/cfd_trade", "cfd_america_ver1.10_short_open_v4_lev_2.py"),
-        # ("C:/workspace/cfd_trade", "cfd_america_ver1.12_open_v4_before_train.py"),
-        ("C:/workspace/cfd_trade", "nihon_ver1.12_open_v5_load_sim_ansemble_bigcompany.py"),
+        ("C:/workspace/cfd_trade", "nihon_ver1.12_open_v5_load_sim_ansemble5_bigcompany_top40.py"),
+        ("C:/workspace/cfd_trade", "nnihon_ver1.12_open_v5_load_sim_ansemble5_bigcompany.py.py"),
         # ("C:/workspace/cfd_trade", "nihon_ver1.12_open_v4_load_sim_nomalcompany.py"),
         # ("C:/workspace/cfd_trade", "america_ver1.12_open_v5_load_sim.py"),
         # ("C:/workspace/cfd_trade", "cfd_america_ver1.11_Fernandes_lev5.py"),
